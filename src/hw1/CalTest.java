30a3344275b35f9f868ea5d274893cf1c1f344e6
Sean Huang <playpcfun@gmail.com>
2024-04-29 21:44:44 +0800

    renew hw6

--- src/hw1/CalTest.java
+++ src/hw1/CalTest.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class CalTest {
 	public static void main(String[]args) throws CalException {
-		Scanner s =new Scanner(System.in);
-		System.out.println("請輸入x的值");
-		int x = s.nextInt();
-		System.out.println("請輸入y的值");
-		int y = s.nextInt();
-		
-		Calculator c = new Calculator();		
-		System.out.println(c.powerXY(x, y));
+		Scanner sc =new Scanner(System.in);
+		Calculator c = new Calculator();
+		int ary[] = new int [2];
+		int i = 0;
+		while(i<=0) {
+			try {
+			System.out.println("請輸入x的值");
+			ary[0] = sc.nextInt();
+			System.out.println("請輸入y的值");
+			ary[1] = sc.nextInt();
+			System.out.println(ary[0]+"的"+ary[1]+"次方等於"+c.powerXY(ary[0], ary[1]));
+			i++;			
+			}catch(Exception e) {
+				System.out.println("輸入格式不正確");
+				sc.next();
+			}			
+		}		
 	}
 }
--- src/hw1/Calculator.java
+++ src/hw1/Calculator.java
@@ -2,13 +2,15 @@ package hw1;
 
 public class Calculator {
 	public int powerXY(int x, int y) throws CalException {
-		int result;
+		int result = 0;
+		
+		result = (int) Math.pow(x, y);			
 		if(x == 0 && y == 0) 
 			throw new CalException("0的0次方沒有意義!");		
 		else if(y<0)
 			throw new CalException("次方為負值，結果回傳不為整數!");
-		else	
-		result = (int) Math.pow(x, y);
+		
+		
 		return result;
 		
 	}
