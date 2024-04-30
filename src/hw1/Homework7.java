1f1c3f9d5413493f934f9c9cc3794f6106d7a2d3
Sean Huang <playpcfun@gmail.com>
2024-05-01 01:09:50 +0800

    renew hw7

--- src/hw1/Homework7.java
+++ src/hw1/Homework7.java
@@ -1,11 +1,35 @@
 package hw1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Homework7 {
-	public static void main(String[]args) {
-		File f1 = new File("c:\\Sample.txt");
-		System.out.println(
-				f1.getName()+"檔案共有"+f1.length()+"個位元組");
+	public static void main(String[]args) {		
+		int countChar = 0;
+		int countLine = 0;		
+		try {
+			File sp = new File("Sample.txt");
+			FileReader fr = new FileReader(sp);
+			BufferedReader br = new BufferedReader(fr);
+			String data;
+			
+			while((data=br.readLine())!=null) {			
+				countChar += data.length();
+				countLine++;
+				
+			}
+			
+			System.out.println(
+					sp.getName()+"檔案共有"+sp.length()+"個位元組"
+					+countChar+"個字元"+countLine+"列資料"
+					);
+			
+			br.close();
+			fr.close();
+		}catch(IOException e){
+			
+		}
 	}
 }
